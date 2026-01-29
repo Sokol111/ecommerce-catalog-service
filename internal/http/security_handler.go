25b0309b15730fb1830489c283a82492714c718e
Sokol111 <igorsokol111@gmail.com>
2026-01-30 00:20:02 +0100

    u

--- internal/http/security_handler.go
+++ internal/http/security_handler.go
@@ -8,15 +8,15 @@ import (
 )
 
 type securityHandler struct {
-	tokenValidator token.TokenValidator
+	tokenValidator token.Validator
 }
 
-func newSecurityHandler(tokenValidator token.TokenValidator) httpapi.SecurityHandler {
+func newSecurityHandler(tokenValidator token.Validator) httpapi.SecurityHandler {
 	return &securityHandler{tokenValidator: tokenValidator}
 }
 
 // HandleBearerAuth handles BearerAuth security.
 func (s *securityHandler) HandleBearerAuth(ctx context.Context, operationName httpapi.OperationName, t httpapi.BearerAuth) (context.Context, error) {
-	ctx, _, err := token.HandleBearerAuth(s.tokenValidator, ctx, t.Token, t.Roles)
+	ctx, _, err := token.HandleBearerAuth(ctx, s.tokenValidator, t.Token, t.Roles)
 	return ctx, err
 }
